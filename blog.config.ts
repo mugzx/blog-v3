d1fd81d14eb1eea32cb6a101791ca3c4e20ff79c
mugzx <apollo@mugzx.top>
2025-01-30 23:30:58 +0800

    Follow验证: 成功

--- blog.config.ts
+++ blog.config.ts
@@ -5,8 +5,8 @@ import { zhCN } from 'date-fns/locale'
 // 存储 nuxt.config 和 app.config 共用的配置
 const blogConfig = {
     title: '地球驿站',
-    subtitle: 'feedId:107805652515466240+userId:107805269913881600',
-    description: 'feedId:107805652515466240+userId:107805269913881600',
+    subtitle: '希望,寄托在你们身上。',
+    description: '木姜丝的个人博客，目前分享马哲与生活。这个博客记录了他在生活和学习中的点滴经历。网站界面开源自纸鹿,简洁美观是该开源项目的特点之一，且为读者提供了卓越的阅读体验。',
     author: {
         name: '木姜丝_Mugzx',
         avatar: 'https://q1.qlogo.cn/g?b=qq&nk=2111829348&src_uin=www.jlwz.cn&s=0',
